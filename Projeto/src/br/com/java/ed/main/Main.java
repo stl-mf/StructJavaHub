bb8a61d41969de52d8ccf4cb9a5c08f9b18d1022
estelah <emfj.estela@gmail.com>
2024-06-14 19:25:32 -0300

    Updates

--- Projeto/src/br/com/java/ed/main/Main.java
+++ Projeto/src/br/com/java/ed/main/Main.java
@@ -2,30 +2,40 @@ package br.com.java.ed.main;
 
 import java.util.Scanner;
 
-public class Main {
-	public static void main (String[] args) {
+import br.com.java.ed.implementation.Supermarket;
+import br.com.java.ed.implementation.SupermarketArray;
+
+public class Main { //classe executavel
+	private final static int SIZE = 3;
+	
+	public static void main (String[] args) { //método main
 		Scanner scanner = new Scanner(System.in); //ler o teclado que o usuario vai indicar
 		
+		Supermarket supermarket = new SupermarketArray(SIZE);
+		
 		int opcao; //armazenando em uma variável
-		do {
+		do { //laço
 			System.out.println("\nLista de Compras");
 			System.out.println("1 - Inserir");
 			System.out.println("2 - Listar");
 			System.out.println("3 - Remover");
 			System.out.println("4 - Sair");
 			System.out.println("Escolha uma opção: ");
-			opcao = scanner.nextInt();
+			opcao = scanner.nextInt(); //aguardando a opção do usuário final
 			
 			switch (opcao) {
 			case 1:
-					System.out.print("Digite o item a ser inserido");
-					String item = scanner.next();
-					break;
+				System.out.print("Digite o item a ser inserido: ");
+				String item = scanner.next();
+				supermarket.add(item);
+				break;
 			case 2:
+				supermarket.print();
 				break;
 			case 3:
 				System.out.print("Digite a posição do item a ser removido: ");
 				int index = scanner.nextInt();
+				supermarket.delete(index);
 				break;
 			case 4:
 				System.out.println("Saindo do programa...");
